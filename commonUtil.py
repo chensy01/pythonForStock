4b51e49681604773c1ce1760416c8742e2467cd3
jack <csy.mailbox@qq.com>
2017-06-09 22:36:41 +0800

    新增新的获取方式

--- commonUtil.py
+++ commonUtil.py
@@ -6,7 +6,12 @@ from sqlalchemy import create_engine
 import tushare as ts
 import time,datetime
 
-
+engine = create_engine('mysql://jack:jack@127.0.0.1/jack?charset=utf8')
+dbhostip='localhost'
+dbport=3306
+dbuser='jack'
+dbpasswd='jack'
+dbname='jack'
 def queryStockCount(stock):
 
 	conn = MySQLdb.connect(host='localhost', port= 3306, user='jack', passwd='jack',db='jack')
@@ -29,7 +34,7 @@ def queryStockCount(stock):
 
 def queryStockMaxTradeDate(stock):
 	try:
-		conn = MySQLdb.connect(host='localhost', port = 3306, user='jack', passwd='jack', db='jack')
+		conn = MySQLdb.connect(host=dbhostip, port = dbport, user=dbuser, passwd=dbpasswd, db=dbname)
 		cur = conn.cursor()
 		cur.execute("select max(date) from quote_data_qfq_new where stock = '" + stock + "'")
 		maxDate = cur.fetchone()
@@ -41,9 +46,13 @@ def queryStockMaxTradeDate(stock):
 		pass
 	finally:
 		pass
+	if maxDate is None:
+		maxDate = [datetime.datetime(1986, 9, 30, 0, 0, 0, 0)]  
 	cur.close
 	conn.commit()
 	conn.close()
+	if(maxDate[0] is None):
+		maxDate[0] = datetime.datetime(1986, 9, 30, 0, 0, 0, 0)
 	return maxDate[0] + datetime.timedelta(days=1)
 
 
@@ -60,7 +69,7 @@ def downloadQuoteByStockAndDate(tRealbegin, tRealend, stock):
 
 		try:
 			df = ts.get_h_data(stock, start=strRealbegin, end=strRealend)
-			engine = create_engine('mysql://jack:jack@127.0.0.1/jack?charset=utf8')
+			
 			#存入数据库
 			#df.to_sql('tick_data',engine)
 			#追加数据到现有表
@@ -68,15 +77,60 @@ def downloadQuoteByStockAndDate(tRealbegin, tRealend, stock):
 			df.to_sql('quote_data_qfq_new',engine,if_exists='append')
 		except Exception as e:
 			print "exception found" + str(e)
+			df = None
 		else:
 			pass
 		finally:
 			print '\n'
 		
-		if df is None:
-			print stock + ' does not have ' + strRealbegin + ' to ' + strRealend + ' data'
-			continue
+			if df is None:
+				print stock + ' does not have ' + strRealbegin + ' to ' + strRealend + ' data'
+			#	continue
+
+def downloadQuoteByStock(stock):
+	now = datetime.datetime.now()
+	strBegin = queryStockMaxTradeDateInDaily(stock).strftime('%Y-%m-%d')
+	strEnd = now.strftime('%Y-%m-%d')
+	try:
+		df = ts.get_k_data(code=stock, ktype='D', autype='qfq', start=strBegin, end=strEnd)
+		df.to_sql('stock_daily_quote_qfq',engine,if_exists='append')
+	except Exception as e:
+		print "exception found " + str(e)
+		df = None
+	else:
+		pass
+	finally:
+		print '\n'
+	if df is None:
+		print 'stock ' + stock + ' does not have quote data'
+	else:
+		print 'stock ' + stock + ' download completed'
 
 def log(msg):
 	nowtime = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
 	print   nowtime + ':' + msg 
+
+
+def queryStockMaxTradeDateInDaily(stock):
+	try:
+		conn = MySQLdb.connect(host=dbhostip, port = dbport, user=dbuser, passwd=dbpasswd, db=dbname)
+		cur = conn.cursor()
+		cur.execute("select max(date) from stock_daily_quote_qfq where code = '" + stock + "'")
+		maxDate = cur.fetchone()
+		#maxDate = tmaxDate[0].strftime('%Y-%m-%d')
+		print maxDate
+	except Exception as e:
+		print str(e)
+		maxDate = [datetime.datetime(1986, 9, 30, 0, 0, 0, 0)]
+	else:
+		pass
+	finally:
+		pass
+	if maxDate is None:
+		maxDate = [datetime.datetime(1986, 9, 30, 0, 0, 0, 0)]  
+	cur.close
+	conn.commit()
+	conn.close()
+	if(maxDate[0] is None):
+		return  datetime.datetime(1986, 9, 30, 0, 0, 0, 0)
+	return maxDate[0] + datetime.timedelta(days=1)
